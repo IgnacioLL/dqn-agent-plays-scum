42167f07a82bc05d280b83c1c4cf5ff75ea25fef
Ignacio Lloret <ig.lloret.l@gmail.com>
2024-09-01 12:42:40 +0200

    Add Constants class for card game configuration
    - Define constants for the number of suits, cards per suit, and total number of cards in a deck.
    - Include a dictionary mapping the number of cards to their corresponding textual representation.

---
+++ constants.py
@@ -0,0 +1,6 @@
+class Constants:
+    NUMBER_OF_SUITS = 4
+    NUMBER_OF_CARDS_PER_SUIT = 13
+    NUMBER_OF_CARDS = NUMBER_OF_SUITS * NUMBER_OF_CARDS_PER_SUIT
+
+    N_CARDS_TO_TEXT = {0:'a', 1: 'a pair of', 2: 'a triplet of', 3: 'a quad of'}
