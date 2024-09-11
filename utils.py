874608a78082954bf32c3cbc7087e4dfb532d895
Ignacio Lloret <ig.lloret.l@gmail.com>
2024-09-11 12:53:04 +0100

    Return as tensor rather than np.array

--- utils.py
+++ utils.py
@@ -1,8 +1,10 @@
 import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
 import numpy as np
+import torch
+from constants import Constants as C
 
-def convert_to_binary(data: list[list[int]]) -> np.array:
+def convert_to_binary_tensor(data: list[list[int]], pass_option: bool = False) -> torch.tensor:
     result = [[0] * 14 for _ in range(4)]
 
     # Convert each subarray (after the first) to a set for O(1) membership checks
@@ -14,14 +16,17 @@ def convert_to_binary(data: list[list[int]]) -> np.array:
             # Check in each subsequent set
             if i in subarray_sets[j]:
                 result[j][i-1] = 1
-    return np.array(result).flatten()
+    if pass_option:
+        return torch.tensor(np.append(np.array(result).flatten(), 1)).float().to(C.DEVICE)
+    else:
+        return torch.tensor(np.append(np.array(result).flatten(), 0)).float().to(C.DEVICE)
 
 def print_rl_variables(reward: int, new_observation: np.array, finish: bool, epsilon: float) -> None:
         print(f"The reward is: {reward}")
         print(f"The new observation is: {new_observation}")
         print(f"The finish is: {finish}")
         print(f"The epsilon is: {epsilon}")
-        
+
 if __name__ == "__main__":
     test = [[1,2,3,4,5,6,7,8,9, 9, 9,11,11], [9,11], [9], []]
     binary_result = convert_to_binary(test)
