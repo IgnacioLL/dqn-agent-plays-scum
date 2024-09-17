75e01e827bb2344537e762bb5c96d10f419886c5
Ignacio Lloret <ig.lloret.l@gmail.com>
2024-09-17 15:42:47 +0100

    Save models and stats in tensorboard and models folders

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from constants import Constants as C
 from env import ScumEnv
 from tqdm import tqdm 
 import numpy as np
-from utils import print_rl_variables
-import torch
+from torch.utils.tensorboard import SummaryWriter
+writer = SummaryWriter()
 
 def main():
     env = ScumEnv(5)
     agents = [DQNAgent(epsilon=C.EPSILON, learning_rate=0.0001) for _ in range(5)]
     ep_rewards = [[] for _ in range(5)]
-
+    max_average_reward = [-np.inf for _ in range(5)]
 
     for episode in tqdm(range(1, C.EPISODES + 1), ascii=True, unit='episodes'):
         finish_agents = [False] * 5
@@ -28,7 +28,7 @@ def main():
             agent = agents[env.player_turn]
             action_state = env.get_cards_to_play()
             action = env.decide_move(action_state, epsilon=agent.epsilon, agent=agent)
-            env._print_move(action)
+            
             current_state, new_state, reward, finish, agent_number = env.make_move(action)
             finish_agents[agent_number] = finish
 
@@ -45,22 +45,26 @@ def main():
 
             agent.train(finish)
             step += 1
-            print("Number of players finished: ", finish_agents)
 
         # Append episode rewards to the lists and log stats
         for i in range(5):
             ep_rewards[i].append(episode_rewards[i])
-        if not episode % C.AGGREGATE_STATS_EVERY or episode == 1:
+        if not episode % C.AGGREGATE_STATS_EVERY:
             for i in range(5):
                 average_reward = sum(ep_rewards[i][-C.AGGREGATE_STATS_EVERY:]) / len(ep_rewards[i][-C.AGGREGATE_STATS_EVERY:])
                 min_reward = min(ep_rewards[i][-C.AGGREGATE_STATS_EVERY:])
                 max_reward = max(ep_rewards[i][-C.AGGREGATE_STATS_EVERY:])
                 print(f"Agent {i+1}: Avg: {average_reward:.2f}, Min: {min_reward:.2f}, Max: {max_reward:.2f} with epsilon {agents[i].epsilon} and learning rate {agents[i].learning_rate}")
-
+                writer.add_scalar(f"Agent {i+1}/Avg", average_reward, episode)
+                writer.flush()
+                if average_reward > max_average_reward[i]:
+                    max_average_reward[i] = average_reward
+                    agents[i].save_model(path=f"models/best_models/agent_{i+1}_episode_{episode}_max_avg_{max_average_reward[i]:.2f}.pt")
+                else:
+                    agents[i].save_model(path=f"models/checkpoints/agent_{i+1}_episode_{episode}_max_avg_{max_average_reward[i]:.2f}.pt")
         # Decay epsilon for all agents
         # For now we will only train the first agent if it works, then we will train the others
         agents[0].decay_epsilon()
         
 if __name__ == "__main__":
     main()
-
