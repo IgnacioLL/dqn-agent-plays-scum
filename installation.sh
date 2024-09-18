96d801ffbfdd852a8fce64549b1f1833f6a8ff2f
Ubuntu <ubuntu@ip-172-31-38-185.us-east-2.compute.internal>
2024-09-18 17:33:17 +0000

    Create installation of miniconda

---
+++ installation.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Download the latest Miniconda installer
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+
+# Install Miniconda silently
+bash miniconda.sh -b -p $HOME/miniconda
+
+# Remove the installer
+rm miniconda.sh
+
+# Initialize conda for bash shell
+$HOME/miniconda/bin/conda init bash
+
+# Activate installation
+source $HOME/.bashrc
+
+echo "Miniconda has been installed silently."
